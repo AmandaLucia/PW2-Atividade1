9f775b403f79a60d0f95e5323a9cb09e3051d609
AmandaLucia <amanda_guarachi@hotmail.com>
2020-03-11 21:57:38 -0300

    Commit de método ehDiaDaSemana

--- PW2.ATIVIDADE1/PW2 Atividade/src/ExercicioUm.java
+++ PW2.ATIVIDADE1/PW2 Atividade/src/ExercicioUm.java
@@ -1,4 +1,11 @@
 
 public class ExercicioUm {
+	public static boolean ehDiaDaSemana (String palavra) {
 
+		return!(palavra.equalsIgnoreCase("Sábado") || palavra.equalsIgnoreCase("Domingo"));
+		}
+	public static void main(String[] args) {
+		boolean  DiaValido = ehDiaDaSemana("Segunda-Feira");
+				System.out.println("É um dia da semana: " + DiaValido);
+	}
 }
